f1bfd73110eef2b8c3dfcd751be0a318fa66c451
Lancelot <Lancelot@Lancelot.local>
2013-11-20 14:41:26 +0900

    initially added

---
+++ src/testApp.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "ofMain.h"
+#include "ofxOpenCv.h"
+#include "particle.h"
+#define NUM 10000
+
+class testApp : public ofBaseApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    ofVideoPlayer vidPlayer;
+    
+    ofxCvColorImage colorImg;
+    ofxCvColorImage colorImgHSV;
+    ofxCvColorImage colorImgRGB;
+    
+    ofxCvGrayscaleImage grayImage;
+    ofxCvGrayscaleImage grayBg;
+    ofxCvGrayscaleImage grayDiff;
+    ofxCvContourFinder contourFinder;
+    
+    ofxCvColorImage compositeImg;
+    
+    int threshold;
+    bool bLearnBakground;
+    
+    int nPixels = 480*312;
+    float r[480*312];
+    float g[480*312];
+    float b[480*312];
+    unsigned char* compositeImgPixels = new unsigned char[nPixels*3];
+    
+    int time1 = 0;
+    int time2 = 0;
+    int time3 = 0;
+    int time4 = 0;
+    
+    bool end0 = true;
+    bool end1 = false;
+    bool end2 = false;
+    bool end3 = false;
+    bool end4 = false;
+
+    particle p[NUM];
+};
+
